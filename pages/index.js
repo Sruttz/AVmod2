faa690994b31fe7d9207e0a34b0e9ff8bcc574c7
Sruttz <110161415+Sruttz@users.noreply.github.com>
2023-06-05 16:21:27 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -59,17 +59,17 @@ export default function HomePage() {
     }
   }
 
-  const deposit = async() => {
+  const avg = async() => {
     if (atm) {
-      let tx = await atm.deposit(1);
+      let tx = await atm.average(10,20,30,40,50);
       await tx.wait()
       getBalance();
     }
   }
 
-  const withdraw = async() => {
+  const secbutton = async() => {
     if (atm) {
-      let tx = await atm.withdraw(1);
+      let tx = await atm.sec(10,20,30,40,50);
       await tx.wait()
       getBalance();
     }
@@ -94,8 +94,8 @@ export default function HomePage() {
       <div>
         <p>Your Account: {account}</p>
         <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <button onClick={avg}>Average of 10,20,30,40,50</button>
+        <button onClick={secbutton}>Divide average by 10</button>
       </div>
     )
   }
@@ -104,11 +104,13 @@ export default function HomePage() {
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header><h1>Sruthika's Frontend</h1></header>
       {initUser()}
       <style jsx>{`
         .container {
-          text-align: center
+          text-align: center;
+          background-color:pink;
+          color:white;
         }
       `}
       </style>
